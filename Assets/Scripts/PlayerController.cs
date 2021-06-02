e7e354779083aca7508173eb57432371fc15853f
qwdasfd <80808149+qwdasfd@users.noreply.github.com>
2021-06-02 15:06:58 +0900

    06/02

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -37,5 +37,11 @@ public class PlayerController : MonoBehaviour
             ani.SetTrigger("damage");
             GameManager.instance.AddScore(-1);
         }
+
+        if (collision.collider.tag == "Enermy")
+        {
+            ani.SetTrigger("attack01");
+            GameManager.instance.OnPlayerDead();
+        }
     }
 }
